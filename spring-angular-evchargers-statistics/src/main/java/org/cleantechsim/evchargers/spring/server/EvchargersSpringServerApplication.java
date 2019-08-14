1179f3ff261bb7f4f494b86f9d250bfef41cfdaf
CleanTechSim Developer <dev@cleantechsim.org>
2019-08-14 15:21:53 +0200

    Initial version of spring/angular EV charger statistics web application

---
+++ spring-angular-evchargers-statistics/src/main/java/org/cleantechsim/evchargers/spring/server/EvchargersSpringServerApplication.java
@@ -0,0 +1,13 @@
+package org.cleantechsim.evchargers.spring.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvchargersSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvchargersSpringServerApplication.class, args);
+	}
+
+}
