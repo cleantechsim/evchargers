3f638c97c6ff5f483dddd76ea8cf3d61eade537b
CleanTechSim Developer <dev@cleantechsim.org>
2019-09-02 16:48:59 +0200

    Task #83, removed commented out code from markers.js

--- python-flask-reactjs-map/templates/public/js/markers.js
+++ python-flask-reactjs-map/templates/public/js/markers.js
@@ -101,16 +101,6 @@ function _createSVGMarker(placement, count) {
     };
 
     return result;
-
-
-    /*
-                var svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-                svgElement.setAttribute('xmlns', "http://www.w3.org/2000/svg");
-                svgElement.setAttribute('viewBox', "0 0 200 200");
-                svgElement.innerHTML = '<rect width="200" height="200"/><rect x="75" y="23" width="50" height="50" style="fill:red"/><rect x="75" y="123" width="50" height="50" style="fill:#0013ff"/>';
-                var svgElementBounds = [[32, -130], [13, -100]];
-                return L.svgOverlay(svgElement, svgElementBounds);
-                */
 }
 
 function updateMarkerSizeOnZoom(map, allMarkers, markerWidthInPixels) {
