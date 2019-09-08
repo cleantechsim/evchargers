20c3fa1432ce58ea95fe8fb42b7839028a459f1e
CleanTechSim Developer <dev@cleantechsim.org>
2019-09-08 19:07:16 +0200

    Task #85, python wrapper module

---
+++ python-flask-reactjs-map/geo_clustering/c/python_module.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+
+#include <Python.h>
+
+#include "geo_clustering.h"
+
+static PyObject *merge_aggregations_wrapper(PyObject *self, PyObject *args) {
+
+    PyObject *list;
+
+    float max_diameter_km;
+
+    if (!PyArg_ParseTuple(args, "Of", &list, &max_diameter_km)) {
+        printf("No arg\n");
+        return NULL;
+    }
+
+    const uint32_t len = PyList_Size(list);
+
+    geo_input_point_t *input_points = malloc(len * sizeof(geo_input_point_t));
+
+    PyObject *result_list;
+
+    PyObject *module_name = PyString_FromString("__main__");
+
+    PyObject *module = PyImport_Import(module_name);
+
+    Py_DECREF(module_name);
+
+    PyObject *module_dict = PyModule_GetDict(module);
+
+    PyObject *cls = PyDict_GetItemString(module_dict, "TestPoint");
+
+    if (input_points != NULL) {
+
+        scratch_buf_t out_points;
+
+        if (scratch_buf_init(&out_points, len, sizeof (geo_merged_point_t))) {
+
+            for (int i = 0; i < len; ++ i) {
+                PyObject *clustered_point = PyList_GetItem(list, i);
+
+                PyObject *count = PyObject_GetAttrString(clustered_point, "count");
+
+                printf("value %ld\n", PyInt_AsLong(count));
+
+                PyObject *geo_point = PyObject_CallMethod(clustered_point, "get_point", NULL);
+                PyObject *latitude_object = PyObject_GetAttrString(geo_point, "latitude");
+
+                PyObject *longitude_object = PyObject_GetAttrString(geo_point, "longitude");
+
+                input_points[i].count = PyInt_AsLong(count);
+                input_points[i].geo_point.latitude = PyFloat_AsDouble(latitude_object);
+                input_points[i].geo_point.longitude = PyFloat_AsDouble(longitude_object);
+
+                Py_DECREF(count);
+                Py_DECREF(geo_point);
+                Py_DECREF(latitude_object);
+                Py_DECREF(longitude_object);
+            }
+
+            const int32_t merged = merge_aggregations(0, input_points, len, max_diameter_km, &out_points);
+
+            if (merged >= 0) {
+
+                result_list = PyList_New(0);
+
+                for (uint32_t i = 0; i < merged; ++ i) {
+
+                    const geo_merged_point_t *const points = out_points.buf;
+
+printf("## initializing point\n");
+                    PyObject *params = Py_BuildValue(
+                                        "iff",
+                                        points[i].count,
+                                        points[i].geo_point.latitude,
+                                        points[i].geo_point.longitude);
+
+                    PyObject *result_point = PyObject_CallObject(
+                                                            cls,
+                                                            params);
+
+                    // Py_DECREF(params);
+
+                    if (result_point == NULL) {
+                        printf("null result point");
+                    }
+
+                    PyList_Append(result_list, result_point);
+                }
+            }
+
+            scratch_buf_free(&out_points);
+        }
+
+        free(input_points);
+
+    }
+
+    Py_DECREF(cls);
+
+    /* Py_DECREF(module_dict); */
+
+    Py_DECREF(list);
+
+    printf("returning list %p\n", result_list);
+
+    if (result_list != NULL) {
+        return result_list;
+    }
+
+    Py_RETURN_NONE;
+}
+
+static PyMethodDef geo_clustering_methods [] = {
+    { "merge_aggregations", (PyCFunction)merge_aggregations_wrapper, METH_VARARGS, NULL },
+    { NULL, NULL, 0, NULL }
+};
+
+PyMODINIT_FUNC initgeo_clustering() {
+
+    Py_InitModule3("geo_clustering", geo_clustering_methods, "Test method");
+
+}
+
---
+++ python-flask-reactjs-map/geo_clustering/c/test.py
@@ -0,0 +1,44 @@
+
+import sys
+
+sys.path.append('.')
+
+from geo_clustering import merge_aggregations
+
+class GeoPoint:
+
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __str__(self):
+        return '(' + str(self.latitude) + ', ' + str(self.longitude) + ')'        
+
+
+class TestPoint:
+
+    def __init__(self, count, latitude, longitude):
+        self.count = count
+        self.point = GeoPoint(latitude, longitude)
+
+    def get_point(self):
+        return self.point
+
+    def __str__(self):
+        return '' # 'count=' + str(self.count) + ', point=' + str(self.point)
+
+
+points = [TestPoint(123, 1.5, 3.5)]
+
+result = merge_aggregations(points, 2000.0)
+
+if result == None:
+    print('result is None')
+
+else:
+
+    for point in result:
+        print('got result ' + str(point.get_point().latitude))
+
+
+
