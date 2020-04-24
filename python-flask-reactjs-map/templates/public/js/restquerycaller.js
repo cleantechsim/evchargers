68d99637d8a426c7c50557620ed4d5ee192b3a37
CleanTechSim Developer <dev@cleantechsim.org>
2020-04-24 18:58:15 +0000

    Fixed sequence counter issue

--- python-flask-reactjs-map/templates/public/js/restquerycaller.js
+++ python-flask-reactjs-map/templates/public/js/restquerycaller.js
@@ -1,24 +1,26 @@
 
 /*
-Ensures that if user quickly moves or zooms the map multiple times,
-old responses are ignored and one runs the query again.
-
+   Ensures that if user quickly moves or zooms the map multiple times,
+   old responses are ignored and one runs the query again.
 */
 
 function RESTQueryCaller() {
 
-    this.requestSequenceNo = 0;
+    this._requestSequenceNo = 0;
 }
 
 RESTQueryCaller.prototype.callQuery = function(performQuery, onresponse) {
 
-    this._callQuery(performQuery, onresponse, ++ this._requestSequenceNo);
+    var sequenceNo = ++ this._requestSequenceNo;
+
+    this._callQuery(performQuery, onresponse, sequenceNo);
 }
 
 RESTQueryCaller.prototype._callQuery = function(performQuery, onresponse, sequenceNo) {
 
     performQuery(
         responseData => {
+
             if (sequenceNo < this._requestSequenceNo) {
                 // Expired request, new ones sent
             }
